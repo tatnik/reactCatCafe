34c68d904e1ae24e509d972f60154c39cf6b9808
tatnik <ptn48@yandex.ru>
2023-09-03 00:15:27 +0300

    create project

---
+++ src/components/blocks/stars-list/stars-list.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function StarsList() {
+  return <></>;
+}
+
+export default StarsList;
---
+++ src/components/ui/star-card/star-card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function StarCard() {
+  return <></>;
+}
+
+export default StarCard;
