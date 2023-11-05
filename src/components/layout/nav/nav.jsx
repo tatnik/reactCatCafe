c165e3c31e00684c608060b3e89d3f03204418b7
tatnik <ptn48@yandex.ru>
2023-11-05 23:52:41 +0300

    navigation for BuyPage

--- src/components/layout/nav/nav.jsx
+++ src/components/layout/nav/nav.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import Button from "../../ui/button/button.jsx";
+import { AppRoute } from "../../../const.js";
+import { useLocation } from "react-router-dom";
+import { StyledButton } from "./styles.js";
+
+const buttons = [
+  {
+    to: AppRoute.MAIN,
+    button: (
+      <StyledButton minWidth={260} key={AppRoute.MAIN} link={AppRoute.MAIN}>
+        Главная
+      </StyledButton>
+    )
+  },
+  {
+    to: `/${AppRoute.BUY}`,
+    button: (
+      <Button minWidth={260} key={AppRoute.BUY} link={AppRoute.BUY}>
+        Купить билет
+      </Button>
+    )
+  }
+];
 
 function Nav() {
+  const pageUrl = useLocation().pathname;
   return (
     <nav>
-      <Button minWidth={260} link="/buy">Купить билет</Button>
+      <nav>
+        {buttons
+          .filter((button) => button.to !== pageUrl)
+          .map((button) => button.button)}
+      </nav>
     </nav>
   );
 }
