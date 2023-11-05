e7a14eeea6b9221e69d1a22cce060fb67532c389
tatnik <ptn48@yandex.ru>
2023-11-05 21:07:14 +0300

    add routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,32 @@
 import PageWrapper from './../layout/page-wrapper/page-wrapper';
 import starsList from "../../mocks/stars-list"
-import { GlobalStyle } from '../../styles';
+import { GlobalStyle } from './styles';
 import gallery from '../../mocks/gallery';
 import buyOptions from './../../mocks/buy-options';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppRoute } from '../../const';
+import MainPage from './../pages/main-page/main-page';
+import BuyPage from './../pages/buy-page/buy-page';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <PageWrapper stars={starsList} slides={gallery} buyOptions={buyOptions} />
+      <Router>
+        <Routes>
+          <Route
+            path={AppRoute.MAIN}
+            element={<PageWrapper />}>
+            <Route
+              index
+              element={<MainPage stars={starsList} />} />
+            <Route
+              path={AppRoute.BUY}
+              element={<BuyPage slides={gallery} buyOptions={buyOptions} />} />
+          </Route>
+        </Routes>
+
+      </Router>
     </>
   );
 }
--- src/components/layout/page-wrapper/page-wrapper.jsx
+++ src/components/layout/page-wrapper/page-wrapper.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Header from "../header/header";
-import MainPage from "../../pages/main-page/main-page";
 import Footer from "../footer/footer";
 import { Main } from "./styles";
-import BuyPage from "../../pages/buy-page/buy-page";
+import { Outlet } from "react-router-dom";
 
 
-
-function PageWrapper({ children, ...prop }) {
+function PageWrapper() {
   return (
     <>
       <Header />
       <Main>
-        <BuyPage {...prop} />
+        <Outlet />
       </Main>
       <Footer />
     </>
